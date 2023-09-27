5c9b26028a7c63fdcb612e8868b4577d6ebe6615
Ravil <star.toni@mail.ru>
2023-09-27 09:04:15 +0600

    [TASK] Resources -> Added (Fonts, AssetHelp, AppHelp) -> (AppColor, AppImage, Colors)

---
+++ Nomad/Resources/AppHelp/AppColor.swift
@@ -0,0 +1,31 @@
+//
+//  AppColor.swift
+//  Moony Journey
+//
+//  Created by Ravil on 27.09.2023.
+//
+
+import Foundation
+import UIKit
+
+protocol AppColorProtocol {
+    var rawValue: String { get }
+}
+
+extension AppColorProtocol {
+
+    var uiColor: UIColor {
+        guard let color = UIColor.init(named: rawValue) else {
+            fatalError("Could not find color with name \(rawValue)")
+        }
+        return color
+    }
+
+    var cgColor: CGColor {
+        return uiColor.cgColor
+    }
+}
+
+enum AppColor: String, AppColorProtocol {
+    case white
+}
---
+++ Nomad/Resources/AppHelp/AppImage.swift
@@ -0,0 +1,39 @@
+//
+//  AppImage.swift
+//  Moony Journey
+//
+//  Created by Ravil on 27.09.2023.
+//
+
+import Foundation
+import UIKit
+
+protocol AppImageProtocol {
+    var rawValue: String { get }
+}
+
+extension AppImageProtocol {
+
+    var uiImage: UIImage? {
+        guard let image = UIImage(named: rawValue) else {
+            fatalError("Could not find image with name \(rawValue)")
+        }
+        return image
+    }
+    
+    var systemImage: UIImage? {
+        guard let image = UIImage(systemName: rawValue) else {
+            fatalError("Could not find image with name \(rawValue)")
+        }
+        return image
+    }
+    
+}
+
+enum AppImage: String, AppImageProtocol {
+    
+    // MARK: - Main
+    
+    case background
+    
+}
