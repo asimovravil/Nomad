0b17a01574b8ab15e28bd680de6653ff8c720162
Ravil <star.toni@mail.ru>
2023-09-27 10:28:19 +0600

    [TASK] LevelsModule -> WIP

---
+++ Nomad/Controllers/LevelsModule/LevelsViewController.swift
@@ -0,0 +1,40 @@
+//
+//  LevelsViewController.swift
+//  Nomad
+//
+//  Created by Ravil on 27.09.2023.
+//
+
+import UIKit
+import SnapKit
+
+final class LevelsViewController: UIViewController {
+
+    // MARK: - UI
+    
+    // MARK: - Lifecycle
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        setupViews()
+        setupConstraints()
+        setupNavigationBar()
+    }
+        
+    // MARK: - setupViews
+        
+    private func setupViews() {
+    }
+
+    // MARK: - setupConstraints
+        
+    private func setupConstraints() {
+
+    }
+    
+    // MARK: - setupNavigationBar
+        
+    private func setupNavigationBar() {
+    }
+}
